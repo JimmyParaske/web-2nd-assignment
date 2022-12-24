5b64b68c4acc5b53329a80dbf8e2e9e107d029ae
JimmyParaske <jimmykpd21@yahoo.com>
2022-12-24 17:33:44 +0200

    2o paradoteo done

--- public/js/category.js
+++ public/js/category.js
@@ -33,6 +33,7 @@ fetch("https://wiki-shop.onrender.com/categories/" + id + "/products")
         
             
             `{{#each this}} 
+                <div class="{{subcategory_id}}">
                     <div class='proion'>
                         <div class="title">
                             <h2>{{title}}</h2>
@@ -56,16 +57,20 @@ fetch("https://wiki-shop.onrender.com/categories/" + id + "/products")
                             <p>{{cost}}.99$</p>
                         </div>
                     </div>
-                {{/each}}`);
+               </div>
+            {{/each}}`);
     
     
             let content = template.templateFunction(data);
             let products = document.getElementById("myProducts");
-            products.innerHTML += content;
+            let teliko = products;
+
+            teliko.innerHTML += content;
+           
     })
     .catch(error => console.log(error));
    
-
+   
 
 
 
@@ -80,11 +85,11 @@ fetch("https://wiki-shop.onrender.com/categories/" + id + "/subcategories")
             `<aside class='kathgories-proiontwn'>
                
                 <h3>Choose subcategory:</h3>
-                    <input type="radio" id="0" name="subcategory-button" class="ypokathgories" value="0">
+                    <input type="radio" id="All" name="subcategory-button" class="ypokathgories" value="0" onclick="ShowHideDiv()">
                     <label for="0">All</label>
                     {{#each this}}
                         <div class="koumpia">
-                            <input type="radio" id="{{id}}" name="subcategory-button" class="ypokathgories" value="{{title}}">
+                            <input type="radio" id="{{title}}" name="subcategory-button" class="ypokathgories" value="{{title}}" onclick="ShowHideDiv()">
                             <label for="{{id}}">{{title}}</label>
                         </div>
                     {{/each}}
@@ -92,7 +97,242 @@ fetch("https://wiki-shop.onrender.com/categories/" + id + "/subcategories")
     
     
             let content = template.templateFunction(data);
-            let products = document.getElementById("mySubcategories");
-            products.innerHTML += content;
+            let epiloges = document.getElementById("mySubcategories");
+            epiloges.innerHTML += content;
     })
     .catch(error => console.log(error));
+
+   
+    function ShowHideDiv() {
+        var chk0 = document.getElementById("All");
+        var chk1 = document.getElementById("Hoodie");
+        var chk2 = document.getElementById("Shirt");
+        var chk3 = document.getElementById("Shocks");
+        var chk4 = document.getElementById("T-Shirt");
+        var chk5 = document.getElementById("DrinkWear");
+        var chk6 = document.getElementById("Pens");
+        var chk7 = document.getElementById("Stickers");   
+        var chk8 = document.getElementById("Water Bottle");
+        var chk9 = document.getElementById("Games");
+        var chk10 = document.getElementById("Onesies");
+        
+        var showHoodie = document.getElementsByClassName("1");
+        var showShirt = document.getElementsByClassName("2");
+        var showSocks = document.getElementsByClassName("3");
+        var showTshirt = document.getElementsByClassName("4");
+     
+        var showDrinkWear = document.getElementsByClassName("5");
+        var showPens = document.getElementsByClassName("6");
+        var showStickers = document.getElementsByClassName("7");
+        var showWaterBottle = document.getElementsByClassName("8");
+      
+        var showGames = document.getElementsByClassName("9");
+        var showOnesies  = document.getElementsByClassName("10");
+        
+
+       
+
+        
+
+        if(id == 1){
+            if(chk0.checked){
+                for(let i=0; i < showHoodie.length ; i++){
+                    showHoodie[i].style.display = "block";
+                }
+                for(let i=0; i < showShirt.length ; i++){
+                    showShirt[i].style.display = "block";
+                }
+                for(let i=0; i < showTshirt.length ; i++){
+                    showTshirt[i].style.display = "block";
+                }
+    
+                showSocks[0].style.display = "block";
+    
+                for(let i=0; i < showDrinkWear.length ; i++){
+                    showDrinkWear[i].style.display = "block";
+                }
+                for(let i=0; i < showPens.length ; i++){
+                    showPens[i].style.display = "block";
+                }
+                for(let i=0; i < showStickers.length ; i++){
+                    showStickers[i].style.display = "block";
+                }
+                for(let i=0; i < showWaterBottle.length ; i++){
+                    showWaterBottle[i].style.display = "block";
+                }
+                for(let i=0; i < showGames.length ; i++){
+                    showGames[i].style.display = "block";
+                }
+                for(let i=0; i < showOnesies.length ; i++){
+                    showOnesies[i].style.display = "block";
+                }
+    
+            }
+            else if(chk1.checked){
+                for(let i=0; i < showHoodie.length ; i++){
+                    showHoodie[i].style.display = "block";
+                }
+                for(let i=0; i < showShirt.length ; i++){
+                    showShirt[i].style.display = "none";
+                }
+                for(let i=0; i < showTshirt.length ; i++){
+                    showTshirt[i].style.display = "none";
+                }
+                showSocks[0].style.display = "none";
+            }
+            else if(chk2.checked){
+                for(let i=0; i < showShirt.length ; i++){
+                    showShirt[i].style.display = "block";
+                }
+                for(let i=0; i < showHoodie.length ; i++){
+                    showHoodie[i].style.display = "none";
+                }
+                showSocks[0].style.display = "none";
+                for(let i=0; i < showTshirt.length ; i++){
+                    showTshirt[i].style.display = "none";
+                }
+            }
+            else if(chk3.checked){
+
+                showSocks[0].style.display = "block";
+                for(let i=0; i < showHoodie.length ; i++){
+                    showHoodie[i].style.display = "none";
+                }
+                for(let i=0; i < showShirt.length ; i++){
+                    showShirt[i].style.display = "none";
+                }
+                for(let i=0; i < showTshirt.length ; i++){
+                    showTshirt[i].style.display = "none";
+                }
+
+            }
+            else if(chk4.checked){
+
+                for(let i=0; i < showTshirt.length ; i++){
+                    showTshirt[i].style.display = "block";
+                }
+                for(let i=0; i < showHoodie.length ; i++){
+                    showHoodie[i].style.display = "none";
+                }
+                for(let i=0; i < showShirt.length ; i++){
+                    showShirt[i].style.display = "none";
+                }
+                showSocks[0].style.display = "none";
+
+            }
+        }
+
+        if(id == 2){
+            if(chk0.checked){
+
+                for(let i=0; i < showDrinkWear.length ; i++){
+                    showDrinkWear[i].style.display = "block";
+                }
+                for(let i=0; i < showPens.length ; i++){
+                    showPens[i].style.display = "block";
+                }
+                for(let i=0; i < showStickers.length ; i++){
+                    showStickers[i].style.display = "block";
+                }
+                for(let i=0; i < showWaterBottle.length ; i++){
+                    showWaterBottle[i].style.display = "block";
+                }
+                
+            }
+            if(chk5.checked){
+
+                for(let i=0; i < showDrinkWear.length ; i++){
+                    showDrinkWear[i].style.display = "block";
+                }
+                for(let i=0; i < showPens.length ; i++){
+                    showPens[i].style.display = "none";
+                }
+                for(let i=0; i < showStickers.length ; i++){
+                    showStickers[i].style.display = "none";
+                }
+                for(let i=0; i < showWaterBottle.length ; i++){
+                    showWaterBottle[i].style.display = "none";
+                }
+                
+            }
+            if(chk6.checked){
+
+                for(let i=0; i < showDrinkWear.length ; i++){
+                    showDrinkWear[i].style.display = "none";
+                }
+                for(let i=0; i < showPens.length ; i++){
+                    showPens[i].style.display = "block";
+                }
+                for(let i=0; i < showStickers.length ; i++){
+                    showStickers[i].style.display = "none";
+                }
+                for(let i=0; i < showWaterBottle.length ; i++){
+                    showWaterBottle[i].style.display = "none";
+                }
+            }
+            if(chk7.checked){
+
+                for(let i=0; i < showDrinkWear.length ; i++){
+                    showDrinkWear[i].style.display = "none";
+                }
+                for(let i=0; i < showPens.length ; i++){
+                    showPens[i].style.display = "none";
+                }
+                for(let i=0; i < showStickers.length ; i++){
+                    showStickers[i].style.display = "block";
+                }
+                for(let i=0; i < showWaterBottle.length ; i++){
+                    showWaterBottle[i].style.display = "none";
+                }
+            }
+            if(chk8.checked){
+
+                for(let i=0; i < showDrinkWear.length ; i++){
+                    showDrinkWear[i].style.display = "none";
+                }
+                for(let i=0; i < showPens.length ; i++){
+                    showPens[i].style.display = "none";
+                }
+                for(let i=0; i < showStickers.length ; i++){
+                    showStickers[i].style.display = "none";
+                }
+                for(let i=0; i < showWaterBottle.length ; i++){
+                    showWaterBottle[i].style.display = "block";
+                }
+            }
+        }    
+        
+        if(id == 3){
+
+            if(chk0.checked){
+
+                for(let i=0; i < showGames.length ; i++){
+                    showGames[i].style.display = "block";
+                }
+                for(let i=0; i < showOnesies.length ; i++){
+                    showOnesies[i].style.display = "block";
+                }
+               
+                
+            }
+            else if(chk9.checked){
+                for(let i=0; i < showGames.length ; i++){   
+                    showGames[i].style.display = "block";
+                }
+                for(let i=0; i < showOnesies.length ; i++){
+                    showOnesies[i].style.display = "none";
+                }
+
+            }
+            else if(chk10.checked){
+                for(let i=0; i < showGames.length ; i++){
+                    showGames[i].style.display = "none";
+                }
+                for(let i=0; i < showOnesies.length ; i++){
+                    showOnesies[i].style.display = "block";
+                }
+            }
+
+
+        }
+    }
